24c9d9a2a3c0feded4ca748c0f2affe643b548af
Christina Holt <christinalynnholt@outlook.com>
2026-04-30 18:19:34 -0700

    Switch to HashRouter for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import ProductList from "./components/ProductList";
 import CartPage from "./pages/CartPage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/plants" element={<ProductList />} />
         <Route path="/cart" element={<CartPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
